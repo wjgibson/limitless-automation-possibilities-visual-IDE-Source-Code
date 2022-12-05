139ce4498432015cb6b94c08e8961a20887d6ed4
jacobdye <jadye@bsu.edu>
2022-12-05 04:16:37 -0500

    Create all file to node type dependencies

--- src/resources/nodeTypes.js
+++ src/resources/nodeTypes.js
@@ -1,8 +1,12 @@
 import ControlModuleNode from "../Elements/ControlModuleNode";
 import Caste2Node from "../Elements/Caste2Node";
+import StepNode from "../Elements/StepNode";
+import SequenceNode from "../Elements/SequenceNode";
 
 var nodeTypes = {
   controlModule: ControlModuleNode,
-  caste2: Caste2Node
+  caste2: Caste2Node,
+  step: StepNode,
+  sequence: SequenceNode
 };
 export default nodeTypes;
