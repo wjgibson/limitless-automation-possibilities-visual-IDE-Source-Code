b473ad690b631bb7c8d0dad130fbc10e606101b5
jacobdye <jadye@bsu.edu>
2022-12-06 05:05:06 -0500

    Remove unneeded nodes that were templates

--- src/Elements/Sidebar.js
+++ src/Elements/Sidebar.js
@@ -13,28 +13,12 @@ export default () => {
         You can drag these nodes to the pane on the left.
       </div>
       <div
-        aria-label="input"
-        className="dndnode input"
-        onDragStart={(event) => onDragStart(event, "input")}
-        draggable
-      >
-        Input Node
-      </div>
-      <div
         aria-label="sequence"
         className="dndnode"
         onDragStart={(event) => onDragStart(event, "sequence")}
         draggable
       >
         Sequence
-      </div>
-      <div
-        aria-label="step"
-        className="dndnode"
-        onDragStart={(event) => onDragStart(event, "step")}
-        draggable
-      >
-        Step
       </div>
       <div
         aria-label="controlModule"
@@ -51,14 +35,6 @@ export default () => {
         draggable
       >
         Caste 2 Sequence
-      </div>
-      <div
-        aria-label="output"
-        className="dndnode output"
-        onDragStart={(event) => onDragStart(event, "output")}
-        draggable
-      >
-        Output Node
       </div>
     </aside>
   );
