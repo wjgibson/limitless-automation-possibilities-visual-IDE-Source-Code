5939fe030452efc039c7a1789117bcc9a7971ee6
wjgibson <wjgibson@bsu.edu>
2022-12-06 20:35:58 -0500

    remove console.logs

--- src/resources/Validator.js
+++ src/resources/Validator.js
@@ -2,14 +2,9 @@ function Validator(instance, connection) {
   const sourceNodeLevel = instance.getNode(connection.source).data.seqType;
   const targetNodeLevel = instance.getNode(connection.target).data.seqType;
 
-  console.log(sourceNodeLevel);
-  console.log(targetNodeLevel);
-
   if (targetNodeLevel >= sourceNodeLevel) {
-    console.log("false");
     return false;
   }
-  console.log("true");
   return true;
 }
 
