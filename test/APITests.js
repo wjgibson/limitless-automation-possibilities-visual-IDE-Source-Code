857b40ef2223957e63fb98e3eb3e7fbe61019039
wjgibson <wjgibson@bsu.edu>
2022-12-07 21:48:01 -0500

    Add test folder

--- test/APITests.js
+++ test/APITests.js
@@ -1,7 +1,7 @@
 const request = require("supertest")("http://localhost:3001/");
 const expect = require("chai").expect;
 
-describe("getAllConfigsTest not", () => {
+describe("getAllConfigsTest not empty", () => {
   it("should return a json object", () => {
     return request.get("getAllConfigsTest").then((res) => {
       expect(res.body.data).not.to.be.empty;
