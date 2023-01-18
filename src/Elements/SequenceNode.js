8085d7d6e82015da2fe7dc8a1e1cf7ef37610f98
wjgibson <wjgibson@bsu.edu>
2023-01-18 13:04:01 -0500

    notifies user multiple times if invalid

--- src/Elements/SequenceNode.js
+++ src/Elements/SequenceNode.js
@@ -1,6 +1,6 @@
 import { Handle, Position, useReactFlow } from "reactflow";
 import { React, useState, useEffect } from "react";
-import { Card } from "antd";
+import { Card, message } from "antd";
 import "../Elements/elements.css";
 import DownDownMenu from "./dropDownMenu.js";
 import Validator from "../resources/Validator";
@@ -12,8 +12,13 @@ This is a user defined description for this node
 function SequenceNode({ data }) {
   const reactFlowInstance = useReactFlow();
 
+  const [messageApi, contextHolder] = message.useMessage();
   const [color, setColor] = useState(data.color);
   const [seqType, setSeqType] = useState(data.seqType);
+
+  const info = () => {
+    messageApi.info("This connection is invalid!");
+  };
 
   useEffect(() => {
     console.log(data);
@@ -28,55 +33,68 @@ function SequenceNode({ data }) {
   }, [color]);
 
   function isValidConnection(connection) {
-    return Validator(reactFlowInstance, connection);
+    const connectionValidity = Validator(reactFlowInstance, connection);
+    notifyInvalidConnection(connectionValidity);
+    return connectionValidity;
+  }
+
+  function notifyInvalidConnection(connectionValidity) {
+    if (connectionValidity == false) {
+      {
+        info();
+      }
+    }
   }
 
   data.opcid = 45;
 
   return (
-    <Card
-      title={
-        <div>
-          <h3
-            style={{
-              display: "inline",
-              color: "white",
-              mixBlendMode: "difference",
-            }}
-          >
-            Sequence
-          </h3>
-          <DownDownMenu
-            setSeqLayer={setSeqType}
-            setColor={setColor}
-            style={{ display: "inline", float: "right" }}
-          ></DownDownMenu>
+    <>
+      {contextHolder}
+      <Card
+        title={
+          <div>
+            <h3
+              style={{
+                display: "inline",
+                color: "white",
+                mixBlendMode: "difference",
+              }}
+            >
+              Sequence
+            </h3>
+            <DownDownMenu
+              setSeqLayer={setSeqType}
+              setColor={setColor}
+              style={{ display: "inline", float: "right" }}
+            ></DownDownMenu>
+          </div>
+        }
+        bordered={false}
+        style={{
+          width: 300,
+          backgroundColor: color,
+          mixBlendMode: "difference",
+        }}
+      >
+        <div className="dynamicTextColor">
+          <div>
+            <div>Type: {seqType}</div>
+          </div>
+          <p>Sequence</p>
+          <Handle
+            type="target"
+            position={Position.Top}
+            isValidConnection={isValidConnection}
+          />
+          <Handle
+            type="source"
+            position={Position.Bottom}
+            isValidConnection={isValidConnection}
+          />
         </div>
-      }
-      bordered={false}
-      style={{
-        width: 300,
-        backgroundColor: color,
-        mixBlendMode: "difference",
-      }}
-    >
-      <div className="dynamicTextColor">
-        <div>
-          <div>Type: {seqType}</div>
-        </div>
-        <p>Sequence</p>
-        <Handle
-          type="target"
-          position={Position.Top}
-          isValidConnection={isValidConnection}
-        />
-        <Handle
-          type="source"
-          position={Position.Bottom}
-          isValidConnection={isValidConnection}
-        />
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 }
 
