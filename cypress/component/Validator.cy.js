ae67a04934f0b1dc7f44ce5bd4e29cc7a02aeaa0
wjgibson <wjgibson@bsu.edu>
2023-02-20 11:16:59 -0500

    begin validator testing

---
+++ cypress/component/Validator.cy.js
@@ -0,0 +1,11 @@
+import Validator from "../../src/utilities/Validator";
+
+let connection = {
+  source: "5b07d410-8186-4885-81e5-22438ee07e8f",
+  sourceHandle: null,
+  target: "a76dcda4-d743-44d7-9df7-84407c788c9f",
+  targetHandle: null,
+};
+
+let instance =
+  describe("Validator takes two node levels and returns the validity of them", () => {});
--- src/utilities/Validator.js
+++ src/utilities/Validator.js
@@ -1,12 +1,25 @@
-function Validator(instance, connection) {
-  const sourceNodeLevel = instance
-    .getNode(connection.source)
-    .data.type.split("|")[1];
-  const targetNodeLevel = instance
-    .getNode(connection.target)
-    .data.type.split("|")[1];
+let testingFlag = false;
 
-  console.log(targetNodeLevel);
+function setTestingFlag(bool) {
+  testingFlag = bool;
+  console.log(`new testing flag value is ${testingFlag}`);
+}
+
+function Validator(instance, connection) {
+  let sourceNodeLevel;
+  let targetNodeLevel;
+  console.log(
+    `instance: ${JSON.stringify(instance.getNode(connection.source))}`
+  );
+  console.log(`connection: ${JSON.stringify(connection)}`);
+  if (!testingFlag) {
+    sourceNodeLevel = instance
+      .getNode(connection.source)
+      .data.type.split("|")[1];
+    targetNodeLevel = instance
+      .getNode(connection.target)
+      .data.type.split("|")[1];
+  }
   if (targetNodeLevel == 1) {
     if (sourceNodeLevel == 2) {
       return true;
@@ -18,5 +31,4 @@ function Validator(instance, connection) {
   }
   return true;
 }
-
 export default Validator;
