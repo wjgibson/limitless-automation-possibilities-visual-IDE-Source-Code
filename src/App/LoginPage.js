76a1b2dae20c08534198ff118e649a05071ee219
wesley-kring <wjkring@bsu.edu>
2023-03-30 02:22:26 -0400

    add username and password field

--- src/App/LoginPage.js
+++ src/App/LoginPage.js
@@ -3,9 +3,19 @@ import MainPage from './MainPage.js';
 
 function LoginPage() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   function handleLogin() {
     setIsLoggedIn(true);
+  }
+
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
   }
 
   return (
@@ -13,7 +23,15 @@ function LoginPage() {
       {isLoggedIn ? (
         <MainPage />
       ) : (
-        <button onClick={handleLogin}>Log in</button>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={handleUsernameChange} />
+          <br />
+          <label>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} />
+          <br />
+          <button onClick={handleLogin}>Log in</button>
+        </div>
       )}
     </div>
   );
