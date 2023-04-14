3be8882d7e7cf7abdc87fca3d2935bd9e90b0972
wjgibson <wjgibson@bsu.edu>
2023-04-14 15:21:32 -0400

    Add placeholders and collapse

--- src/Elements/SettingsPage.js
+++ src/Elements/SettingsPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Collapse } from "antd";
+const { Panel } = Collapse;
 import axios from "axios";
 
 const layout = {
@@ -36,78 +37,85 @@ const SettingsPage = () => {
   }
 
   return (
-    <Form {...layout} initialValues={settings} onFinish={onFinish}>
-      <Form.Item
-        label="Database User"
-        name="user"
-        rules={[
-          {
-            required: true,
-            message: "Please input your database user!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <>
+      <h1>Settings</h1>
+      <Collapse accordion>
+        <Panel header="Database Connection Settings" key="1">
+          <Form initialValues={settings} onFinish={onFinish}>
+            <Form.Item
+              label="Database User"
+              name="user"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your database user!",
+                },
+              ]}
+            >
+              <Input placeholder="postgres" />
+            </Form.Item>
 
-      <Form.Item
-        label="Database Host"
-        name="host"
-        rules={[
-          {
-            required: true,
-            message: "Please input your database host!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+            <Form.Item
+              label="Database Host"
+              name="host"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your database host!",
+                },
+              ]}
+            >
+              <Input placeholder="localhost" />
+            </Form.Item>
 
-      <Form.Item
-        label="Database Name"
-        name="database"
-        rules={[
-          {
-            required: true,
-            message: "Please input your database name!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+            <Form.Item
+              label="Database Name"
+              name="database"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your database name!",
+                },
+              ]}
+            >
+              <Input placeholder="LAP_PG" />
+            </Form.Item>
 
-      <Form.Item
-        label="Database Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your database password!",
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+            <Form.Item
+              label="Database Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your database password!",
+                },
+              ]}
+            >
+              <Input.Password placeholder="password" />
+            </Form.Item>
 
-      <Form.Item
-        label="Database Port"
-        name="port"
-        rules={[
-          {
-            required: true,
-            message: "Please input your database port!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+            <Form.Item
+              label="Database Port"
+              name="port"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your database port!",
+                },
+              ]}
+            >
+              <Input placeholder="5432" />
+            </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Save
-        </Button>
-      </Form.Item>
-    </Form>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Save
+              </Button>
+            </Form.Item>
+          </Form>
+        </Panel>
+      </Collapse>
+    </>
   );
 };
 
