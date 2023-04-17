3abaa8eebe33bda415a3e9c05f61cd057455c60a
wjgibson <wjgibson@bsu.edu>
2023-04-17 11:32:05 -0400

    Add a little bit of style to the settings page

--- src/Elements/SettingsPage.js
+++ src/Elements/SettingsPage.js
@@ -8,6 +8,46 @@ const SettingsPage = () => {
   const [settings, setSettings] = useState({});
   const [messageApi, contextHolder] = message.useMessage();
 
+  const styles = {
+    container: {
+      backgroundColor: "#001628",
+      color: "#b4babf",
+      fontFamily: "sans-serif",
+      minHeight: "100vh",
+      padding: "20px",
+      boxSizing: "border-box",
+    },
+    header: {
+      color: "#b4babf",
+      fontSize: "32px",
+      fontWeight: "bold",
+      marginBottom: "20px",
+    },
+    panel: {
+      backgroundColor: "#2f485e",
+      border: "10px",
+      borderRadius: "10px",
+      marginBottom: "20px",
+      color: "#000",
+      background: "#fff",
+    },
+    label: {
+      color: "#b4babf",
+    },
+    input: {
+      backgroundColor: "#b4babf",
+      borderRadius: "5px",
+      color: "#001628",
+      fontWeight: "bold",
+    },
+    button: {
+      backgroundColor: "#b4babf",
+      borderRadius: "5px",
+      color: "#001628",
+      fontWeight: "bold",
+    },
+  };
+
   const displayStatus = (type, content) => {
     messageApi.open({
       type: type,
@@ -38,19 +78,14 @@ const SettingsPage = () => {
   }
 
   return (
-    <div
-    // style={{
-    //   height: "100vh",
-    //   backgroundColor: "#2E475F",
-    // }}
-    >
+    <div style={styles.container}>
       {contextHolder}
-      <h1 style={{ fontFamily: "sans-serif" }}>Settings</h1>
+      <h1 style={styles.header}>Settings</h1>
       <Collapse accordion>
         <Panel
           header="Database Connection Settings"
           key="1"
-          // style={{ color: "white", fontFamily: "sans-serif" }}
+          style={styles.panel}
         >
           <Form initialValues={settings} onFinish={onFinish}>
             <Form.Item
@@ -62,8 +97,9 @@ const SettingsPage = () => {
                   message: "Please input your database user!",
                 },
               ]}
+              style={styles.label}
             >
-              <Input placeholder="postgres" />
+              <Input placeholder="postgres" style={styles.input} />
             </Form.Item>
 
             <Form.Item
@@ -75,8 +111,9 @@ const SettingsPage = () => {
                   message: "Please input your database password!",
                 },
               ]}
+              style={styles.label}
             >
-              <Input.Password placeholder="password" />
+              <Input.Password placeholder="password" style={styles.input} />
             </Form.Item>
 
             <Form.Item
@@ -88,8 +125,9 @@ const SettingsPage = () => {
                   message: "Please input your database name!",
                 },
               ]}
+              style={styles.label}
             >
-              <Input placeholder="LAP_PG" />
+              <Input placeholder="LAP_PG" style={styles.input} />
             </Form.Item>
 
             <Form.Item
@@ -101,8 +139,9 @@ const SettingsPage = () => {
                   message: "Please input your database host!",
                 },
               ]}
+              style={styles.label}
             >
-              <Input placeholder="localhost" />
+              <Input placeholder="localhost" style={styles.input} />
             </Form.Item>
 
             <Form.Item
@@ -114,12 +153,13 @@ const SettingsPage = () => {
                   message: "Please input your database port!",
                 },
               ]}
+              style={styles.label}
             >
-              <Input placeholder="5432" />
+              <Input placeholder="5432" style={styles.input} />
             </Form.Item>
 
             <Form.Item>
-              <Button type="primary" htmlType="submit">
+              <Button type="primary" htmlType="submit" style={styles.button}>
                 Save
               </Button>
             </Form.Item>
@@ -128,7 +168,7 @@ const SettingsPage = () => {
       </Collapse>
       <div style={{ position: "fixed", bottom: "10px", right: "10px" }}>
         <Link to="/main">
-          <Button type="primary" size="large">
+          <Button type="primary" size="large" style={styles.button}>
             Done
           </Button>
         </Link>
